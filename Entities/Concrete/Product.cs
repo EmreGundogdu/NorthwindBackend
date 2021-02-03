6f626357af969ea77e6df407a10f414b86512726
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-02-03 15:11:31 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntitiy
+    {
+        public int ProductId { get; set; }
+        public string PorductName { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
